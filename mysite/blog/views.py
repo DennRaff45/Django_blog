688e95d3903609e97f143f89b7f25879f49dd17d
Denis Salimov <denis_45@bk.ru>
2023-12-13 23:02:34 +0500

    Добавлены шаблоны страниц, и несколь представлений, в том числе отображение постов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+"""Указанная функция извлекает объект, соответствующий передан-
+ным параметрам, либо исключение HTTP с кодом состояния, равным 404 (не
+найдено), если объект не найден"""
+
+
+def post_detail(request):
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post}
+                  )
+
+
+"В этом представлении извлекаются все посты со статусом PUBLISHED используя менеджер published"
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
