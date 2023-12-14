4a18e212bb3f1d6bc6d0125e3e35fc2d0c5ed254
Denis Salimov <denis_45@bk.ru>
2023-12-14 23:38:18 +0500

    Добавлена постраничная разбивка по 3 постам, оторажение URL адресов со слагом

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 """Указанная функция извлекает объект, соответствующий передан-
 ным параметрам, либо исключение HTTP с кодом состояния, равным 404 (не
 найдено), если объект не найден"""
 
 
-def post_detail(request, pk):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                             pk=pk,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request,
                   'blog/post/detail.html',
@@ -22,6 +26,16 @@ def post_detail(request, pk):
 
 def post_list(request):
     posts = Post.published.all()
+    "Постраничная разбивка с 3 постами на страницу"
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        "Если page_number находится вне диапазона, то выдать последнюю страницу"
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
