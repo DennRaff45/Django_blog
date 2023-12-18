c2ae16453db3a2cddd05f3204ce157376ee0c38c
Denis Salimov <denis_45@bk.ru>
2023-12-18 23:14:49 +0500

    Добавлен функционал по отправке сообщений

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
 
 """Указанная функция извлекает объект, соответствующий передан-
 ным параметрам, либо исключение HTTP с кодом состояния, равным 404 (не
@@ -24,18 +28,37 @@ def post_detail(request, year, month, day, post):
 "В этом представлении извлекаются все посты со статусом PUBLISHED используя менеджер published"
 
 
-def post_list(request):
-    posts = Post.published.all()
-    "Постраничная разбивка с 3 постами на страницу"
-    paginator = Paginator(posts, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        "Если page_number находится вне диапазона, то выдать последнюю страницу"
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_pk):
+    "Извлечь пост по id"
+    post = get_object_or_404(Post,
+                             pk=post_pk,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'gromka15@gmail.com',[cd['to']])
+            sent = True
+            # ..отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
