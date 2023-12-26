ab22051dae06f429a8fa05aa73527bb85ce65a3e
Denis Salimov <denis_45@bk.ru>
2023-12-26 23:08:53 +0500

    Добавлены теги, добавлены общие теги по статьям

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,10 +5,32 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 """Указанная функция извлекает объект, соответствующий передан-
 ным параметрам, либо исключение HTTP с кодом состояния, равным 404 (не
 найдено), если объект не найден"""
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -23,11 +45,17 @@ def post_detail(request, year, month, day, post):
     # Форма для комментариев пользователями
     form = CommentForm()
 
+    # Список схожих постов по тегам
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 "В этом представлении извлекаются все посты со статусом PUBLISHED используя менеджер published"
@@ -88,3 +116,5 @@ def post_comment(request, post_pk):
                   {'post': post,
                    'form': form,
                    'comment': comment})
+
+
